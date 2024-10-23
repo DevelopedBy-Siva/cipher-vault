728e7b714d6fdb29c40f2ec8b735d1da368fdc19
DevelopedBy-Siva <developedby.siva@gmail.com>
2024-10-23 19:13:10 +0530

    BugFix: Refresh the table

--- components/table.py
+++ components/table.py
@@ -13,7 +13,8 @@ class Table(ctk.CTkFrame):
         self.__root = root
         self.__parent = parent
         self.__columns = columns
-        self.tree = None
+        self.__tree = None
+        self.__content_container = None
         super().__init__(self.__parent, fg_color=WINDOW["bg"], bg_color=WINDOW["bg"])
         self.__style()
         self.__render_data()  # render rows and columns
@@ -30,10 +31,7 @@ class Table(ctk.CTkFrame):
         # Fetch Data
         error = DataStore.fetch_accounts()
         if not error:
-            if DataStore.account_df.size == 0:
-                self.__notify("No accounts stored in your vault.")
-            else:
-                self.__content()
+            self.__content()
         else:
             self.__notify(error)
 
@@ -65,7 +63,12 @@ class Table(ctk.CTkFrame):
         """
         Table rows
         """
-        container = ctk.CTkScrollableFrame(
+        # If vault is empty, display message
+        if DataStore.account_df.size == 0:
+            self.__notify("No accounts stored in your vault.")
+            return
+
+        self.__content_container = ctk.CTkScrollableFrame(
             self,
             bg_color="transparent",
             fg_color="transparent",
@@ -73,24 +76,26 @@ class Table(ctk.CTkFrame):
             scrollbar_button_color=TEXT["border-light"],
             scrollbar_button_hover_color=TEXT["border-light-hover"],
         )
-        container.grid(column=0, row=1, sticky="news")
+        self.__content_container.grid(column=0, row=1, sticky="news")
 
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.__content_container.grid_rowconfigure(0, weight=1)
+        self.__content_container.grid_columnconfigure(0, weight=1)
 
-        self.tree = ttk.Treeview(container, columns=self.__columns, show="")
-        self.tree.grid(column=0, row=0, sticky="news")
+        self.__tree = ttk.Treeview(
+            self.__content_container, columns=self.__columns, show=""
+        )
+        self.__tree.grid(column=0, row=0, sticky="news")
 
         # Handle row selection
-        self.tree.bind("<<TreeviewSelect>>", self.__open_account)
+        self.__tree.bind("<<TreeviewSelect>>", self.__open_account)
 
         # Handle row hover
-        self.tree.tag_configure("highlight", background=TABLE["bg-hover"])
-        self.tree.bind("<Motion>", self.__highlight_row)
+        self.__tree.tag_configure("highlight", background=TABLE["bg-hover"])
+        self.__tree.bind("<Motion>", self.__highlight_row)
 
         # Create columns
         for col in self.__columns:
-            self.tree.column(col, anchor="w")
+            self.__tree.column(col, anchor="w")
 
         # Insert data to columns
         self.__insert_data()
@@ -99,21 +104,23 @@ class Table(ctk.CTkFrame):
         """
         Insert each data
         """
+        if not self.__tree:
+            return
+
         data = DataStore.select_and_sort(list(self.__columns))
         for idx, (_, row) in enumerate(data.iterrows()):
             row_tag = "odd_row"
             if idx % 2 == 0:
                 row_tag = "even_row"
 
-            if self.tree:
-                self.tree.insert(
-                    "",
-                    "end",
-                    values=[f"   {val}".title() for val in row],
-                    tags=(row_tag,),
-                )
-                self.tree.tag_configure("odd_row", background=TABLE["odd"])
-                self.tree.tag_configure("even_row", background=TABLE["even"])
+            self.__tree.insert(
+                "",
+                "end",
+                values=[f"   {val}".title() for val in row],
+                tags=(row_tag,),
+            )
+            self.__tree.tag_configure("odd_row", background=TABLE["odd"])
+            self.__tree.tag_configure("even_row", background=TABLE["even"])
 
     def __style(self) -> None:
         """
@@ -138,8 +145,8 @@ class Table(ctk.CTkFrame):
         """_summary_
         Render the account details when a selection is made
         """
-        if self.tree and self.tree.selection():
-            Account(self.__root, self.tree)
+        if self.__tree and self.__tree.selection():
+            Account(self.__root, self.__tree)
 
     def __highlight_row(self, event) -> None:
         """
@@ -159,8 +166,8 @@ class Table(ctk.CTkFrame):
 
     def refresh(self) -> None:
         """
-        Refresh data
+        Refresh table content
         """
-        if self.tree:
-            self.tree.delete(*self.tree.get_children())
-        self.__insert_data()
+        if self.__content_container:
+            self.__content_container.destroy()
+        self.__content()
